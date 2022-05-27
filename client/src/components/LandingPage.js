f5d9ce6cfc9b2bfc7f7f99bce02e27188bfdf193
Xiaremba <john.p.zaremba@gmail.com>
2022-05-27 08:53:39 -0400

    Create and style landing page, add background image and styling to app

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,9 +1,14 @@
 import React from "react"
+import { Link } from "react-router-dom";
 
 const LandingPage = (props) => {
   return (
     <div className="landing-page">
-      <h2 className="sign-form box">Hello</h2>
+      <h2 className="landing-page-header">My Easy Tracker</h2>
+      <h5 className="landing-page-subheader">Nutrition tracking made fast, <br /> easy, and accessible</h5>
+      <Link to="/users/new" className="landing-button">
+        Start Tracking Now
+      </Link>
     </div>
   )
 }
