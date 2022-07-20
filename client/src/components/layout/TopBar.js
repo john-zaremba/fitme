838b273ec19e51a35cb831201ca2b20c183d2bfd
Xiaremba <john.p.zaremba@gmail.com>
2022-07-20 16:40:39 -0400

    Create dropdown menu to simplify topbar design

--- client/src/components/layout/TopBar.js
+++ client/src/components/layout/TopBar.js
@@ -1,43 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import SignOutButton from "../authentication/SignOutButton";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faUser, faBook, faRightToBracket, faCheckToSlot } from '@fortawesome/free-solid-svg-icons'
 
 const TopBar = ({ user }) => {
-  const unauthenticatedListItems = [
-    <li key="sign-in">
-      <Link className="sign-in" to="/user-sessions/new">Sign In</Link>
-    </li>,
-    <li key="sign-up">
-      <Link to="/users/new" className="my-button">
-        Sign Up
-      </Link>
-    </li>,
-  ];
+  const [open, setOpen] = useState(false)
+  const handleMenuClick = () => {
+    setOpen(!open)
+  }
 
-  const authenticatedListItems = [
-    <li key="profile">
-      <Link className="sign-in" to="/users/profile">Profile</Link>
-    </li>,
-    <li key="sign-out">
-      <SignOutButton />
-    </li>,
-  ];
+  const unauthenticatedListItems = [
+    <Link 
+      className="menu-item" 
+      to="/users/new" 
+      onClick={handleMenuClick}
+    >
+      <FontAwesomeIcon icon={faCheckToSlot} className="menu-item-icon" /> Sign Up
+    </Link>,
+    <Link 
+      className="menu-item" 
+      to="/user-sessions/new" 
+      onClick={handleMenuClick}
+    >
+      <FontAwesomeIcon icon={faRightToBracket} className="menu-item-icon" /> Sign In
+    </Link>
+  ]
+
+  const authenticatedListItems = [  
+    <Link 
+      className="menu-item" 
+      to="/users/profile" 
+      onClick={handleMenuClick}
+    >
+      <FontAwesomeIcon icon={faUser} className="menu-item-icon" /> Profile
+    </Link>,
+    <Link 
+      className="menu-item"
+      to="/logs" 
+      onClick={handleMenuClick}
+    >
+      <FontAwesomeIcon icon={faBook} className="menu-item-icon" /> My Logs
+    </Link>,
+    <SignOutButton />
+  ]
+
+  const dropdownMenu = (
+    <ul className="dropdown">
+      {user ? authenticatedListItems : unauthenticatedListItems}
+    </ul>
+  )
 
   return (
     <div className="my-top-bar">
       <div className="top-bar-left">
         <ul className="menu">
-          <li className="my-menu-text">My Easy Tracker</li>
-          <li>
-            <Link className="menu-link" to="/">Home</Link>
-          </li>
-          <li>
-            <Link className="menu-link" to="/logs">My Logs</Link>
+          <li className="">
+            <Link className="menu-link" to="/">My Easy Tracker</Link>
           </li>
         </ul>
       </div>
       <div className="top-bar-right">
-        <ul className="menu">{user ? authenticatedListItems : unauthenticatedListItems}</ul>
+        <FontAwesomeIcon
+          className="menu-icon"
+          icon={faBars}
+          onClick={handleMenuClick}
+        />
+        {open && dropdownMenu}
       </div>
     </div>
   );
